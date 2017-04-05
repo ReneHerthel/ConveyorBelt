6cb8385b6d4c28cfd330a120d55c4358f8b83840
thismath <matthis.keppner@haw-hamburg.de>
2017-04-05 19:02:57 +0200

    controller not done yet

--- src/LightSystem/LightSystemController.cpp
+++ src/LightSystem/LightSystemController.cpp
@@ -2,17 +2,33 @@
 // LightSystemController.cpp : Controller sets Lights according to
 // warning level
 // 
-//    Copyright (c) 2017 Stephan Jänecke <stephan.jaenecke@haw-hamburg.de>
+//    Copyright (c) 2017 Stephan Jaenecke <stephan.jaenecke@haw-hamburg.de>
 // =====================================================================================
 
-LightSystemController(LightSystemController* serviceId) :
-serviceId(
-{};
+#include "LightSystemController.h"
 
-void LightSystemService::setLights(Color lightColor, Frequency lightFrequency) {
+
+LightSystemController :: LightSystemController(int chid, ITimer* timer, BLightSystem* boundary)
+	: chid(chid)
+	, timer(timer)
+	, boundary(boundary)
+{}
+
+void LightSystemController::setLights(Color lightColor, Frequency lightFrequency) {
             if ( !lightFrequency ) {
                 halObject.lightOn(lightColor);
             } else {
                 // TODO: Start timer according to frequency
             }
+}
+
+int LightSystemController::task(){
+	// trying to get right access to the I/O hardware
+	if (ThreadCtl(_NTO_TCTL_IO_PRIV, 0) == -1)
+	{
+		std::cout << "Can't get Hardware access, therefore can't do anything."
+				<< std::endl;
+		return EXIT_FAILURE;
+	}
+
 }
