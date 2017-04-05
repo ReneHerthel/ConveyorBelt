37538483c63f1b7ed7ac18214eec7320c3f8bbdd
thismath <matthis.keppner@haw-hamburg.de>
2017-04-05 17:10:05 +0200

    implemented LightSystemHAL

--- src/LightSystem/LightSystemHAL.cpp
+++ src/LightSystem/LightSystemHAL.cpp
@@ -1,34 +1,58 @@
 // =====================================================================================
 // LightSystemHAL.cpp : Provide methods to hide hardware dependend implementation
 // 
-//    Copyright (c) 2017 Stephan Jänecke <stephan.jaenecke@haw-hamburg.de>
+//    Copyright (c) 2017 Stephan Jaenecke <stephan.jaenecke@haw-hamburg.de>
+//						 Matthis Keppner <matthis.keppner@haw-hamburg.de>
 // =====================================================================================
 
 #include "LightSystemHAL.h"
 
+
 namespace HAL {
     void LightSystemHAL::lightOn(Color color) {
-        switch (color) {
+        unsigned char bitMask;
+        bitMask = 0;
+    	switch (color) {
         case Green:
+        	bitMask = GREEN_SHIFT;
+        	break;
         case Yellow:
+        	bitMask = YELLOW_SHIFT;
+        	break;
         case Red:
+        	bitMask = RED_SHIFT;
+        	break;
         default:
-            // TODO: Pass current function
-            stub(color);
+           bitMask = 0;
+           // nothing will happen
         }
+    	out8(CTRL_REG_GROUP0, DEFAULT_PORTS_SETTINGS);
+
+    	unsigned char port_value = in8(PORTA_ADDR);
+    	out8(PORTA_ADDR, (port_value | (1 << bitMask)));
+
     }
 
     void LightSystemHAL::lightOff(Color color) {
-        switch (color) {
+    	unsigned char bitMask;
+    	switch (color) {
         case Green:
-        case Yellow:
-        case Red:
-        default:
-            // TODO: Pass current function
-            stub(color);
-        }
+        	bitMask = GREEN_SHIFT;
+			break;
+		case Yellow:
+			bitMask = YELLOW_SHIFT;
+			break;
+		case Red:
+			bitMask = RED_SHIFT;
+			break;
+		default:
+		   bitMask = 0;
+		   // nothing will happen
+		}
+    	out8(CTRL_REG_GROUP0, DEFAULT_PORTS_SETTINGS);
+
+    	unsigned char port_value = in8(PORTA_ADDR);
+    	out8(PORTA_ADDR, (port_value & ~(1 << bitMask)));
     }
-    void LightSystemHAL::stub(Color color) {
-        std::cout << color << " stub." << std::endl;
-    }
+
 }
--- src/LightSystem/LightSystemHAL.h
+++ src/LightSystem/LightSystemHAL.h
@@ -9,9 +9,31 @@
 
 #include "enum.h"
 #include <iostream>
+#include <hw/inout.h>
+#include <sys/neutrino.h>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
 
+#define GREEN_SHIFT		5
+#define YELLOW_SHIFT	6
+#define RED_SHIFT		7
+#define CTRL_REG_GROUP0     0x303
+/*
+ * PORT A : OUTPUT
+ * PORT B : INPUT
+ * PORT C Lo : OUTPUT
+ * PORT C Hi : INPUT
+ * Mode Select : 00
+ */
+#define DEFAULT_PORTS_SETTINGS 0x8A
+
+#define PORTA_ADDR 0x300
+#define PORTB_ADDR 0x301
+#define INPUT_MASK 0x01
+//TO DO  exclude Port_ADDRS in an other global header
 namespace HAL {
-    class LightSystemHAL {
+    class LightSystemHAL : public BLightSystem {
         public:
             void lightOn(Color color);
             void lightOff(Color color);
