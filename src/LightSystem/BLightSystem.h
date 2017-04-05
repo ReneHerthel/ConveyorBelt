fdb12d9fbb03ec3409d401b6e13b0fb651114e40
thismath <matthis.keppner@haw-hamburg.de>
2017-04-05 17:15:00 +0200

    added Baseclass BLightSystem

---
+++ src/LightSystem/BLightSystem.h
@@ -0,0 +1,20 @@
+/*
+ * BLightSystem.h
+ *
+ *  Created on: 05.04.2017
+ *      Author: Matthis Keppner
+ */
+
+#ifndef BLIGHTSYSTEM_H_
+#define BLIGHTSYSTEM_H_
+
+namespace HAL {
+    class BLightSystem {
+        public:
+            void lightOn(Color color);
+            void lightOff(Color color);
+    };
+}
+
+
+#endif /* BLIGHTSYSTEM_H_ */
