9b50abd5414563b205bbdb77f72be5640a65066d
Sebastian Brueckner <sebastian.brueckner@haw-hamburg.de>
2017-04-12 11:56:27 +0200

    Timer example
    Does not work when fiels uncopied

--- src/examples/Timer/Main.cpp
+++ src/examples/Timer/Main.cpp
@@ -5,16 +5,13 @@
  *      Author: aca619
  */
 #include <iostream>
+#include <chrono>
 #include <sys/neutrino.h>
 #include "ITimer.h"
 #include "TimerService.h"
 
 #define PM_CODE 4
 #define PM_VALUE 123456
-
-static void testThread(int i){
-	std::cout << "Hier ist der Test Thread" << i << std::endl;
-}
 
 int main(void){
 	std::cout << "Geht los hier" << std::endl;
@@ -24,18 +21,26 @@ int main(void){
 		std::cout << "Channel Create failed" << std::endl;
 	}
 
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+
 	struct _pulse pulse;
 
 	ITimer* timer  = new TimerService(chid, PM_CODE, PM_VALUE);
-	timer->setAlarm(9999);
 
+	start = std::chrono::system_clock::now();
+
+	timer->setAlarm(500);
 
 	int err = MsgReceive_r(chid, &pulse, sizeof(_pulse),NULL);
 		if(err) {
 				// TODO error handling
 				std::cout << "client MsgReceive_r failed" << std::endl;
 		}
-	std::cout << "Message received: " << pulse.value.sival_int << std::endl;
+	end = std::chrono::system_clock::now();
+
+	int elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds> (end-start).count();
+
+	std::cout << "Message received: " << pulse.value.sival_int << " after: " << elapsed_seconds << std::endl;
 
 	std::cout << "Zuende hier" << std::endl;
 }
