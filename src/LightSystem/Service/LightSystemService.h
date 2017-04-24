d1b58bf73926eb8d1b6df7d9e210207189f01739
Stephan Jänecke <stephan.jaenecke@haw-hamburg.de>
2017-04-24 16:55:41 +0200

    Begin simplifying service class
    * Remove obsolete private method setLights()

--- src/LightSystem/Service/LightSystemService.h
+++ src/LightSystem/Service/LightSystemService.h
@@ -11,13 +11,17 @@
 #include "LightSystemController.h"
 #include "ILightSystem.h"
 
+struct LightMessage {
+    Color color;
+    Frequency frequency;
+}
+
 class LightSystemService : public ILightSystem {
 	public:
 		void setWarningLevel(Level warningLevel);
 	private:
 		LightSystemController* controller;
 		int channelId;
-        void setLights(Color lightColor, Frequency lightFrequency);
 } ;
 
 #endif
