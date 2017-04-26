58064be0f760d5974183122373017cd6561d69bf
thismath <matthis.keppner@haw-hamburg.de>
2017-04-26 08:45:50 +0200

    states ready in controller

--- src/LightSystem/Controller/LightSystemController.cpp
+++ src/LightSystem/Controller/LightSystemController.cpp
@@ -79,19 +79,86 @@ private:
 
 		virtual void slow_blinking(){
 			boundary.lightOn(YELLOW);
-			setTimer(1000,123); //123 is a bad message FIXME real message
+			setTimer(SLOW_BLINKING,123); //123 is a bad message FIXME real message
 			new (this) Yellow_on;
 		}
 
 	};
 	struct Yellow_on : public State {
 			virtual void Timer_sig(){   //unsure if necessary
-				if(msg)
-				boundary.LightOff(YELLOW);
-				setTimer(1000,)
+				if(msg==12) {
+					boundary.LightOff(YELLOW);
+					setTimer(SLOW_BLINKING,12);
+					new (this) Yellow_off;
+				}
 			}
 	};
+	struct Yellow_off : public State {
+				virtual void Timer_sig(){   //unsure if necessary
+					if(msg==12){
+						boundary.LightOn(YELLOW);
+						setTimer(SLOW_BLINKING,12);
+						new (this) Yellow_on;
+					}
+				}
+	};
 
+	struct Red : public State {
+			virtual void always_on(){   //unsure if necessary
+				boundary.lightOn(RED);
+			}
+			virtual void always_off(){
+				boundary.lightOff(RED);
+			}
+
+			virtual void slow_blinking(){
+				boundary.lightOn(RED);
+				setTimer(SLOW_BLINKING,13); //123 is a bad message FIXME real message
+				new (this) Red_slow_on;
+			}
+			virtual void fast_blinking(){
+				boundary.lightOn(RED);
+				setTimer(FAST_BLINKING,14); //123 is a bad message FIXME real message
+				new (this) Red_fast_on;
+			}
+
+	};
+	struct Red_slow_on : public State {
+		virtual void Timer_sig(){   //unsure if necessary
+			if(msg==13) {
+				boundary.LightOff(RED);
+				setTimer(SLOW_BLINKING,13);
+				new (this) Red_slow_off;
+			}
+		}
+	};
+	struct Red_slow_off : public State {
+		virtual void Timer_sig(){   //unsure if necessary
+			if(msg==13){
+				boundary.LightOn(RED);
+				setTimer(SLOW_BLINKING,13);
+				new (this) Red_slow_on;
+			}
+		}
+	};
+	struct Red_fast_on : public State {
+		virtual void Timer_sig(){   //unsure if necessary
+			if(msg==14) {
+				boundary.LightOff(RED);
+				setTimer(FAST_BLINKING,14);
+				new (this) Red_fast_off;
+			}
+		}
+	};
+	struct Red_fast_off : public State {
+		virtual void Timer_sig(){   //unsure if necessary
+			if(msg==14){
+				boundary.LightOn(RED);
+				setTimer(FAST_BLINKING,14);
+				new (this) Red_fast_on;
+			}
+		}
+	};
 	StateA stateMember;//The memory for the state is part of context object
 	Data contextdata;  //Data is also kept inside the context object
 
