21e7a299054aa53cd9504ca3a83628413979bb81
Jonas Fuhrmann <sebastiab.brueckner@haw-hamburg.de>
2017-05-30 11:40:19 +0200

    Added PuckContext

---
+++ src/PuckManagement/PuckStateMachine/PuckContext.cpp
@@ -0,0 +1,15 @@
+/*
+ * PuckContext.cpp
+ *
+ *  Created on: 30.05.2017
+ *      Author: abz908
+ */
+
+#include "PuckContext.h"
+
+PuckContext::PuckContext() {
+	// TODO Auto-generated constructor stub
+
+}
+
+
---
+++ src/PuckManagement/PuckStateMachine/PuckContext.h
@@ -0,0 +1,16 @@
+/*
+ * PuckContext.h
+ *
+ *  Created on: 30.05.2017
+ *      Author: abz908
+ */
+
+#ifndef PUCKCONTEXT_H_
+#define PUCKCONTEXT_H_
+
+class PuckContext {
+public:
+	PuckContext();
+};
+
+#endif /* PUCKCONTEXT_H_ */
