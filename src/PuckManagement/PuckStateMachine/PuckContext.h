16341e51b503a035547dd67e27df1b5882be47b1
Jonas Fuhrmann <sebastiab.brueckner@haw-hamburg.de>
2017-05-30 13:25:06 +0200

    PuckContext edits

--- src/PuckManagement/PuckStateMachine/PuckContext.h
+++ src/PuckManagement/PuckStateMachine/PuckContext.h
@@ -10,7 +10,64 @@
 
 class PuckContext {
 public:
+
+	enum PuckReturn {
+		ACCEPT,
+		DENY,
+		ERROR,
+		DELETE,
+		SEND,
+		EVALUATE,
+		HEIGHT,
+		SLIDE_FULL
+	};
+
 	PuckContext();
+
+	PuckReturn process(); // todo: pass signal
+	PuckReturn setType(signal_t type);
+
+private:
+	struct PuckState {
+		virtual void inletIn();
+		virtual void inletOut();
+
+		virtual void heightmeasurmentIn();
+		virtual void heightmeasurmentOut();
+
+		virtual void switchIn();
+		virtual void switchOut();
+
+		virtual void slideIn();
+		virtual void slideOut();
+
+		virtual void outletIn();
+		virtual void outletOut();
+
+		virtual void metalDetect();
+
+		virtual void serialReceived();
+
+		virtual void earlyTimer();
+		virtual void lateTimer();
+
+		virtual PuckReturn entry();
+	} *state;
+
+	struct Error : PuckState {
+		PuckReturn entry();
+	};
+
+	struct Inlet : PuckState {
+		void inletIn();
+		PuckReturn entry();
+	};
+
+	struct InletArea : PuckState {
+		void earlyTimer();
+		void heightmeasurmentIn();
+		PuckReturn entry();
+	};
 };
 
 #endif /* PUCKCONTEXT_H_ */
