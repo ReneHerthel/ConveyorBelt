ad0548fa7a68aa7d03d7bcb13e3cbdf4ff5073a2
ReneHerthel <r.herthel@gmail.com>
2017-06-05 19:25:56 +0200

    Recorder: Adds functionality to save records as binary.

--- src/EmbeddedRecorder/BufferFileStreamer.cpp
+++ src/EmbeddedRecorder/BufferFileStreamer.cpp
@@ -14,18 +14,57 @@
  * @author     Rene Herthel <rene.herthel@haw-hamburg.de>
  */
 
+ #include <fstream.h>
+
+
 namespace rec {
 
 BufferFileStreamer::BufferFileStreamer() {
     // TODO
 }
 
-void BufferFileStreamer::exportBuffer() {
-    // TODO
+void BufferFileStreamer::exportBuffer(RecordBuffer * buffer) {
+    ofstream fout;
+    fout.open("records.txt", ios::out | ios::binary | ios::trunc); // Opens the file as binary.
+
+    int ret = 0;
+
+    while (ret >= 0) {
+        record_t record = NULL;
+
+        ret = buffer->read(record);
+
+        if (ret >= 0 && record != NULL) {
+            fout.write((char *)(&record), sizeof(record));
+        }
+    }
+
+    fout << flush;
+    fout.close();
 }
 
-void BufferFileStreamer::importBuffer() {
-    // TODO
+void BufferFileStreamer::importBuffer(RecordBuffer * buffer) {
+    ifstream fin;
+    fin.open("records.txt", ios::in | ios::binary);
+
+    while (1) {
+        record_t record = NULL;
+
+        fin.read(reinterpret_cast<char*> (&record), sizeof(record_t));
+
+        if (fin.eof()) {
+            break;
+        }
+
+        if (record != NULL) {
+            buffer->write(record);
+        }
+        else {
+            break;
+        }
+    }
+
+    fin.close();
 }
 
 } /* namespace rec */
