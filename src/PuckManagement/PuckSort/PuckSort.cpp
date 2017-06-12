02248ec6cf3af37cbe932b1aaeb6f16f1cfe0d53
Stephan Jänecke <stephan.jaenecke@haw-hamburg.de>
2017-06-12 16:17:36 +0200

    Create mock pucksort

---
+++ src/PuckManagement/PuckSort/PuckSort.cpp
@@ -0,0 +1,18 @@
+// =====================================================================================
+//
+//       Filename:  PuckSort.cpp
+//
+//    Description: 
+//
+//        Created:  06/09/2017 05:11:22 PM
+//
+//         Author:  Stephan Jänecke <stephan.jaenecke@haw-hamburg.de>
+//
+// =====================================================================================
+
+#include "PuckSort.h"
+
+bool PuckSort::process(PuckType signal) {
+    /* Keep all pucks for now */
+    return false;
+}
