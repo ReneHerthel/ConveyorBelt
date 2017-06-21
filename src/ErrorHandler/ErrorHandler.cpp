213f3d47ca0eb6575c748d2910867879d2a32c86
ReneHerthel <r.herthel@gmail.com>
2017-06-21 11:08:01 +0200

    ErrorHandler: First framework

---
+++ src/ErrorHandler/ErrorHandler.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 Rene Herthel
+ *
+ * This file is subject to the terms and conditions of the MIT License.
+ * See the file LICENSE in the top level directory for more details.
+ */
+
+/**
+ * @ingroup    error_handler
+ * @{
+ *
+ * @brief      Function declaration of the ErrorHandler.
+ *
+ * @author     Rene Herthel <rene.herthel@haw-hamburg.de>
+ */
+
+ErrorHandler::ErrorHandler()
+{
+
+}
+
+ErrorHandler::~ErrorHandler()
+{
+  
+}
+
+/** @} */
---
+++ src/ErrorHandler/ErrorHandler.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017 Rene Herthel
+ *
+ * This file is subject to the terms and conditions of the MIT License.
+ * See the file LICENSE in the top level directory for more details.
+ */
+
+/**
+ * @ingroup    error_handler
+ * @{
+ *
+ * @brief      Header declaration of the ErrorHandler.
+ *
+ * @author     Rene Herthel <rene.herthel@haw-hamburg.de>
+ */
+
+#ifndef SRC_ERRORHANDLER_ERRORHANDLER_H_
+#define SRC_ERRORHANDLER_ERRORHANDLER_H_
+
+class ErrorHandler {
+public:
+    /*
+     * @brief Default constructor.
+     */
+    ErrorHandler();
+
+    /*
+     * @brief Default destructor.
+     */
+    ~ErrorHandler();
+private:
+}
+
+#endif /* SRC_ERRORHANDLER_ERRORHANDLER_H_ */
+/** @} */
