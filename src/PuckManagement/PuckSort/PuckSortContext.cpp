80444e9cc050d2eb9e343de9c95099479dd68cc0
Stephan Jänecke <stephan.jaenecke@haw-hamburg.de>
2017-06-27 10:46:14 +0200

    PuckSortContext: Process slide empty signals

--- src/PuckManagement/PuckSort/PuckSortContext.cpp
+++ src/PuckManagement/PuckSort/PuckSortContext.cpp
@@ -100,28 +100,51 @@ bool PuckSortContext::process(PuckType signal) {
 #endif
 }
 
-/* SlideFull */
+/* Slide */
 void PuckSortContext::process(PuckReturn message) {
-	if (SLIDE_FULL == message) {
 
-	#ifdef VARIANT_Belt0
-			statePtr->rampe1IsEmpty = false;
-	#else
-			statePtr->rampe0IsEmpty = false;
-	#endif
-		}
-}
-
-/* SLIDE_FULL_SER */
-void PuckSortContext::process(Serial_n::ser_proto_msg message) {
-	if (SLIDE_FULL_VAL == message) {
-#if MACHINE
-		/* FIXME: Probably unnecessary */
+	switch (message) {
+	case SLIDE_FULL:
+#ifdef VARIANT_Belt0
 		statePtr->rampe0IsEmpty = false;
 #else
 		statePtr->rampe1IsEmpty = false;
 #endif
-	}
+		break;
+	case SLIDE_EMPTY:
+#ifdef VARIANT_Belt0
+		statePtr->rampe0IsEmpty = true;
+#else
+		statePtr->rampe1IsEmpty = true;
+#endif
+		break;
+	default:
+		/* Ignore unhandled messages */
+		;
+	};
+}
+
+/* SLIDE_SER */
+void PuckSortContext::process(Serial_n::ser_proto_msg message) {
+	switch (message) {
+	case Serial_n::SLIDE_FULL_SER:
+#ifdef VARIANT_Belt0
+		statePtr->rampe1IsEmpty = false;
+#else
+		statePtr->rampe0IsEmpty = false;
+#endif
+		break;
+	case Serial_n::SLIDE_EMTPY_SER:
+#ifdef VARIANT_Belt0
+		statePtr->rampe1IsEmpty = true;
+#else
+		statePtr->rampe0IsEmpty = true;
+#endif
+		break;
+	default:
+		/* Ignore unhandled messages */
+		;
+	};
 }
 
 void PuckSortContext::PuckSort::logConditionals(void) {
